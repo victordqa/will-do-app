47e12e0899c7e2aa0e984cffb8aeae804e8144a4
victordqa <victorqalves@gmail,com>
2020-11-02 07:57:13 -0400

    User registration w simple validation

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,13 +1,34 @@
-const User = require('../models/User.model')
-const express = require('express')
+const User = require("../models/User.model");
+const express = require("express");
+const bcrypt = require("bcrypt");
+const router = express.Router();
 
-route = express.Router()
-
-//* Route:  POST /api/users/register
+//* Route:  POST /api/users/add
 //* Descr:  Register new user
 //* Access: Public
-route.post('/register', await (req, res)=>{})
+router.post("/add", async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
 
+    //Validations
+    if (!username || !email || !password) {
+      return res.status(400).json({ msg: "Please, enter all fields" });
+    }
 
+    //Check if new user has a unique email adress
 
-module.exports = route
+    let isRegistred = await User.findOne({ email });
+    if (isRegistred) {
+      return res.status(400).json({ msg: "User already registred" });
+    }
+    //Create new user
+    let newUser = new User({ username, email, password });
+    await newUser.save();
+    res.json({ msg: "User created!" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "Server error" });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 mongoose.connect(process.env.DB_HOST, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 //Check pending connection
@@ -15,12 +16,18 @@ const db = mongoose.connection;
 db.on("error", () => console.log("Couldn't connect to MongoDB"));
 db.once("open", () => console.log("Connection to DB succsessful!"));
 
+//Load express
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//Load express json parser
+app.use(express.json());
+
+//Import routes
+const userRoute = require("./routes/user.route");
+
+//Mount routes on express app
+app.use("/api/users", userRoute);
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
