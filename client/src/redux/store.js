507f0e94fde507d8ee35849727c7257aca08a613
Victor de Queiroz Alves <victorqalves@gmail.com>
2020-12-10 18:23:42 -0300

    Removed redux devtools extension for deployment

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,10 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers/index";
-import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './reducers/index'
+import thunk from 'redux-thunk'
 
-let middleWare = [thunk];
+let middleWare = [thunk]
 
 export const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+    rootReducer,
+    compose(applyMiddleware(...middleWare))
+)
