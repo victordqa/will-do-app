892ad479cefd91ffbc9af8be2dc23b31b21c1d36
Victor de Queiroz Alves <victorqalves@gmail.com>
2020-12-11 18:34:09 -0400

    added proxy

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -1,125 +1,118 @@
 import {
-  LOADING_USER,
-  LOADING_USER_SUCCSESS,
-  AUTH_ERROR,
-  LOGIN_SUCCSESS,
-  LOGIN_FAILURE,
-  REGISTER_SUCCSESS,
-  REGISTER_FAILURE,
-  LOGOUT_SUCCSESS,
-} from "../actions/types";
-import { getErrorsAction } from "./errorActions";
-import { getTasksAction } from "./taskActions";
-import axios from "axios";
+    LOADING_USER,
+    LOADING_USER_SUCCSESS,
+    AUTH_ERROR,
+    LOGIN_SUCCSESS,
+    LOGIN_FAILURE,
+    REGISTER_SUCCSESS,
+    REGISTER_FAILURE,
+    LOGOUT_SUCCSESS,
+} from '../actions/types'
+import { getErrorsAction } from './errorActions'
+import { getTasksAction } from './taskActions'
+import axios from 'axios'
 
 //This action keeps sending a token to the server to confirm authentication
 // every time App component rerenders
 export const loadUser = () => async (dispatch, getState) => {
-  // Set user state to loading
-  dispatch(loadingUserAction());
-  try {
-    let res = await axios.get(
-      "http://localhost:5000/api/auth/user",
-      tokenConfig(getState)
-    );
-    dispatch(loadingUserSuccsessAction(res.data));
-    dispatch(getTasksAction());
-  } catch (e) {
-    console.error(e);
-    dispatch(getErrorsAction(e.response.data.msg, e.response.status, "id"));
-    dispatch(authErrorAction());
-  }
-};
+    // Set user state to loading
+    dispatch(loadingUserAction())
+    try {
+        let res = await axios.get('/api/auth/user', tokenConfig(getState))
+        dispatch(loadingUserSuccsessAction(res.data))
+        dispatch(getTasksAction())
+    } catch (e) {
+        console.error(e)
+        dispatch(getErrorsAction(e.response.data.msg, e.response.status, 'id'))
+        dispatch(authErrorAction())
+    }
+}
 
 //Configure request header, add a token from local storage, if any.
 export const tokenConfig = (getState) => {
-  //Get token from redux, if any
-  let token = getState().auth.token;
-  let config = {
-    headers: { "content-type": "application/json" },
-  };
+    //Get token from redux, if any
+    let token = getState().auth.token
+    let config = {
+        headers: { 'content-type': 'application/json' },
+    }
 
-  //if there is token, add it to headers
-  if (token) {
-    config.headers["x-auth-token"] = token;
-  }
-  return config;
-};
+    //if there is token, add it to headers
+    if (token) {
+        config.headers['x-auth-token'] = token
+    }
+    return config
+}
 
 export const loadingUserAction = () => {
-  return { type: LOADING_USER };
-};
+    return { type: LOADING_USER }
+}
 
 export const authErrorAction = () => {
-  return { type: AUTH_ERROR };
-};
+    return { type: AUTH_ERROR }
+}
 
 export const loadingUserSuccsessAction = (user) => {
-  return {
-    type: LOADING_USER_SUCCSESS,
-    payload: user,
-  };
-};
+    return {
+        type: LOADING_USER_SUCCSESS,
+        payload: user,
+    }
+}
 
 export const logInSuccsessAction = (user) => {
-  return {
-    type: LOGIN_SUCCSESS,
-    payload: user,
-  };
-};
+    return {
+        type: LOGIN_SUCCSESS,
+        payload: user,
+    }
+}
 
 export const registerAction = ({ username, email, password }) => async (
-  dispatch,
-  getState
+    dispatch,
+    getState
 ) => {
-  dispatch(loadingUserAction());
+    dispatch(loadingUserAction())
 
-  let data = JSON.stringify({ username, email, password });
-  try {
-    let res = await axios.post(
-      "http://localhost:5000/api/user/add",
-      data,
-      tokenConfig(getState)
-    );
-    dispatch(registerSuccsessAction(res.data));
-  } catch (e) {
-    dispatch({ type: REGISTER_FAILURE });
-    dispatch(
-      getErrorsAction(
-        e.response.data.msg,
-        e.response.status,
-        "REGISTER_FAILURE"
-      )
-    );
-  }
-};
+    let data = JSON.stringify({ username, email, password })
+    try {
+        let res = await axios.post('/api/user/add', data, tokenConfig(getState))
+        dispatch(registerSuccsessAction(res.data))
+    } catch (e) {
+        dispatch({ type: REGISTER_FAILURE })
+        dispatch(
+            getErrorsAction(
+                e.response.data.msg,
+                e.response.status,
+                'REGISTER_FAILURE'
+            )
+        )
+    }
+}
 
 export const logInAction = ({ email, password }) => async (
-  dispatch,
-  getState
+    dispatch,
+    getState
 ) => {
-  dispatch(loadingUserAction());
+    dispatch(loadingUserAction())
 
-  let data = JSON.stringify({ email, password });
-  try {
-    let res = await axios.post(
-      "http://localhost:5000/api/auth",
-      data,
-      tokenConfig(getState)
-    );
-    dispatch(logInSuccsessAction(res.data));
-  } catch (e) {
-    dispatch({ type: LOGIN_FAILURE });
-    dispatch(
-      getErrorsAction(e.response.data.msg, e.response.status, "LOGIN_FAILURE")
-    );
-  }
-};
+    let data = JSON.stringify({ email, password })
+    try {
+        let res = await axios.post('/api/auth', data, tokenConfig(getState))
+        dispatch(logInSuccsessAction(res.data))
+    } catch (e) {
+        dispatch({ type: LOGIN_FAILURE })
+        dispatch(
+            getErrorsAction(
+                e.response.data.msg,
+                e.response.status,
+                'LOGIN_FAILURE'
+            )
+        )
+    }
+}
 
 export const logOutSuccsessAction = () => {
-  return { type: LOGOUT_SUCCSESS };
-};
+    return { type: LOGOUT_SUCCSESS }
+}
 
 export const registerSuccsessAction = (data) => {
-  return { type: REGISTER_SUCCSESS, payload: data };
-};
+    return { type: REGISTER_SUCCSESS, payload: data }
+}
--- client/src/redux/actions/taskActions.js
+++ client/src/redux/actions/taskActions.js
@@ -1,95 +1,96 @@
 import {
-  ADD_TASK_SUCCSESS,
-  DELETE_TASK_SUCCSESS,
-  LOADING_TASKS_SUCCSESS,
-  LOADING_TASKS,
-  CLEAR_SUCCSESS_MESSAGES,
-} from "./types";
-import axios from "axios";
-import { tokenConfig } from "./authActions";
-import { getErrorsAction } from "./errorActions";
+    ADD_TASK_SUCCSESS,
+    DELETE_TASK_SUCCSESS,
+    LOADING_TASKS_SUCCSESS,
+    LOADING_TASKS,
+    CLEAR_SUCCSESS_MESSAGES,
+} from './types'
+import axios from 'axios'
+import { tokenConfig } from './authActions'
+import { getErrorsAction } from './errorActions'
 
 //Send a request to GET all tasks of a specific user based on token's payload (userId)
 export const getTasksAction = () => async (dispatch, getState) => {
-  dispatch(loadingTasksAction());
-  try {
-    let res = await axios.get(
-      "http://localhost:5000/api/task/user_tasks",
-      tokenConfig(getState)
-    );
-    dispatch(loadingTasksSuccsessAction(res.data));
-  } catch (e) {
-    console.error(e.message);
-    dispatch(
-      getErrorsAction(e.response.data.msg, e.response.status, "GET_TASKS_ERROR")
-    );
-  }
-};
+    dispatch(loadingTasksAction())
+    try {
+        let res = await axios.get('/api/task/user_tasks', tokenConfig(getState))
+        dispatch(loadingTasksSuccsessAction(res.data))
+    } catch (e) {
+        console.error(e.message)
+        dispatch(
+            getErrorsAction(
+                e.response.data.msg,
+                e.response.status,
+                'GET_TASKS_ERROR'
+            )
+        )
+    }
+}
 
 export const deleteTaskAction = (taskId) => async (dispatch, getState) => {
-  dispatch(loadingTasksAction());
+    dispatch(loadingTasksAction())
 
-  let data = { taskId };
-  let config = tokenConfig(getState);
+    let data = { taskId }
+    let config = tokenConfig(getState)
 
-  try {
-    //DELETE requests have to be configured this way as opposed to GET and POST
-    let res = await axios({
-      method: "delete",
-      url: "http://localhost:5000/api/task/delete",
-      data: data,
-      ...config,
-    });
-    dispatch(deleteTaskSuccsessAction(res.data));
-    dispatch(getTasksAction());
-  } catch (e) {
-    console.error(e.message);
-    dispatch(
-      getErrorsAction(
-        e.response.data.msg,
-        e.response.status,
-        "DELETE_TASK_ERROR"
-      )
-    );
-  }
-};
+    try {
+        //DELETE requests have to be configured this way as opposed to GET and POST
+        let res = await axios({
+            method: 'delete',
+            url: '/api/task/delete',
+            data: data,
+            ...config,
+        })
+        dispatch(deleteTaskSuccsessAction(res.data))
+        dispatch(getTasksAction())
+    } catch (e) {
+        console.error(e.message)
+        dispatch(
+            getErrorsAction(
+                e.response.data.msg,
+                e.response.status,
+                'DELETE_TASK_ERROR'
+            )
+        )
+    }
+}
 
 export const addTaskAction = (newTask) => async (dispatch, getState) => {
-  dispatch(loadingTasksAction());
-  let data = newTask;
-  let config = tokenConfig(getState);
-  try {
-    let res = await axios.post(
-      "http://localhost:5000/api/task/add",
-      data,
-      config
-    );
-    dispatch(addTaskSuccsessAction(res.data));
-    dispatch(getTasksAction());
-  } catch (e) {
-    console.error(e.message);
-    dispatch(
-      getErrorsAction(e.response.data.msg, e.response.status, "ADD_TASKS_ERROR")
-    );
-  }
-};
+    dispatch(loadingTasksAction())
+    let data = newTask
+    let config = tokenConfig(getState)
+    try {
+        let res = await axios.post('/api/task/add', data, config)
+        dispatch(addTaskSuccsessAction(res.data))
+        dispatch(getTasksAction())
+    } catch (e) {
+        console.error(e.message)
+        dispatch(
+            getErrorsAction(
+                e.response.data.msg,
+                e.response.status,
+                'ADD_TASKS_ERROR'
+            )
+        )
+    }
+}
 
 export const loadingTasksAction = () => {
-  return { type: LOADING_TASKS };
-};
+    return { type: LOADING_TASKS }
+}
 
 export const loadingTasksSuccsessAction = (data) => {
-  return { type: LOADING_TASKS_SUCCSESS, payload: data };
-};
+    return { type: LOADING_TASKS_SUCCSESS, payload: data }
+}
 
 export const addTaskSuccsessAction = (data) => {
-  return { type: ADD_TASK_SUCCSESS, payload: data };
-};
+    return { type: ADD_TASK_SUCCSESS, payload: data }
+}
 
 export const deleteTaskSuccsessAction = (data) => {
-  return { type: DELETE_TASK_SUCCSESS, payload: data };
-};
+    return { type: DELETE_TASK_SUCCSESS, payload: data }
+}
 
 export const clearSuccsessMessagesAction = () => {
-  return { type: CLEAR_SUCCSESS_MESSAGES };
-};
+    return { type: CLEAR_SUCCSESS_MESSAGES }
+}
